a670bc583d3ccc746674d2547862db4fb0f1a56e
Fatih Ünlü <unlu.fa@gmail.com>
2024-08-11 20:36:40 +0300

    add ag-grid

--- src/components/CoinGrid.vue
+++ src/components/CoinGrid.vue
@@ -1,11 +1,31 @@
 <script setup>
 import { ref } from "vue";
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+import { AgGridVue } from "ag-grid-vue3"; // Vue Data Grid Component
 
+const rowData = ref([
+  { id: "coin 1", symbol: "c", price: 64950 },
+  { id: "coin 2", symbol: "c", price: 33850 },
+  { id: "coin 3", symbol: "c", price: 29600 },
+]);
+
+// Column Definitions: Defines the columns to be displayed.
+const colDefs = ref([
+  { field: "id" },
+  { field: "symbol" },
+  { field: "price" },
+]);
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline bg-blue-400">Coins!</h1>
+  <ag-grid-vue
+    :rowData="rowData"
+    :columnDefs="colDefs"
+    style="height: 500px"
+    class="ag-theme-quartz"
+  >
+  </ag-grid-vue>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
