11b8227ff5f687522beb10b2e9857c8667cd59a7
Fatih Ünlü <unlu.fa@gmail.com>
2024-08-25 17:07:18 +0300

    add pinia

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
+
 import App from './App.vue'
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-createApp(App).use(VueAxios, axios).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(VueAxios, axios)
+app.mount('#app')
