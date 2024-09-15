14dfc77beb6c2e8f9aa2205d3f865710c14dc99d
Fatih Ünlü <unlu.fa@gmail.com>
2024-09-15 18:56:19 +0300

    update CoinCellRenderer

--- src/components/cell-renderers/CoinCellRenderer.vue
+++ src/components/cell-renderers/CoinCellRenderer.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="coinCell">
-    <div class="flex items-center w-20">
+    <div>
+    <a
+      href="#"
+      @click.prevent="goToCoinDetail"
+      class="text-blue-500 hover:underline"
+    >
       <img
         :src="data.image"
-        :alt="data.name"
-        class="w-8 h-8 rounded-full mr-2"
+        alt="coin icon"
+        class="w-6 h-6 inline-block mr-2 align-middle"
       />
-      <span>{{ data.name }}</span>
-    </div>
+      <span class="align-middle">{{ data.name }}</span>
+    </a>
+  </div>
   </div>
 </template>
 
 <script setup>
 import { defineProps } from "vue";
+import { useRouter } from 'vue-router';
 
 const { params } = defineProps(["params"]);
-
+const router = useRouter();
 const data = params.data;
+
+function goToCoinDetail() {
+  router.push({ name: 'coin-detail', params: { id: params.data.id } });
+}
 </script>
